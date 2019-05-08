6e6f7e0bf624a6cca7604a35938e68afa3ad083d
ats-12 <ats-12@ats12>
2019-05-08 12:17:01 +0530

    //Mahesh

--- src/main/java/com/ats/triladmin/repository/mrn/ErpHeaderRepository.java
+++ src/main/java/com/ats/triladmin/repository/mrn/ErpHeaderRepository.java
@@ -10,7 +10,7 @@ import com.ats.triladmin.model.ErpHeader;
 import com.ats.triladmin.model.mrn.MrnHeader;
 
 public interface ErpHeaderRepository extends JpaRepository<ErpHeader, Integer> {
-	@Query(value=("SELECT t_mrn_header.mrn_id,t_mrn_header.mrn_no,t_mrn_header.mrn_date,t_mrn_header.bill_no,t_mrn_header.bill_date,m_vendor.vendor_id,t_mrn_header.mrn_status,m_vendor.vendor_name FROM t_mrn_header,m_vendor WHERE m_vendor.vendor_id=t_mrn_header.vendor_id And t_mrn_header.mrn_date BETWEEN :fromDate AND :toDate And t_mrn_header.mrn_status IN(:Status) ORDER BY t_mrn_header.mrn_id"),nativeQuery=true)
+	@Query(value=("SELECT t_mrn_header.mrn_id,t_mrn_header.mrn_no,t_mrn_header.mrn_date,t_mrn_header.bill_no,t_mrn_header.bill_date,m_vendor.vendor_id,t_mrn_header.mrn_status,m_vendor.vendor_name FROM t_mrn_header,m_vendor WHERE m_vendor.vendor_id=t_mrn_header.vendor_id And t_mrn_header.mrn_date BETWEEN :fromDate AND :toDate And t_mrn_header.mrn_status IN(:Status) ORDER BY t_mrn_header.bill_date,t_mrn_header.mrn_id"),nativeQuery=true)
 	List<ErpHeader> getErpHeader(@Param("fromDate")String fromDate, @Param("toDate") String toDate,@Param("Status") List<Integer> Status);
 
 
