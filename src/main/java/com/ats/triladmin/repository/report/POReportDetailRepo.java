39ac5c947c01ed3f89b6ee75e5b96633d775fe3f
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-10-02 11:10:26 +0530

    /Akshay

--- src/main/java/com/ats/triladmin/repository/report/POReportDetailRepo.java
+++ src/main/java/com/ats/triladmin/repository/report/POReportDetailRepo.java
@@ -10,7 +10,7 @@ import com.ats.triladmin.model.report.POReportDetail;
 
 public interface POReportDetailRepo extends JpaRepository<POReportDetail, Integer> {
 	@Query(value = "SELECT po_detail.*,i.item_code,i.item_desc as item_name, ih.ind_m_date FROM po_detail ,m_item i ,indtrans id,indent ih WHERE po_detail.po_id IN(:poIdList)  AND i.item_id=po_detail.item_id AND "
-			+ "po_detail.ind_id=id.ind_d_id and id.ind_m_id=ih.ind_m_id", nativeQuery = true)
+			+ "po_detail.ind_id=id.ind_d_id and id.ind_m_id=ih.ind_m_id and po_detail.status in (0,1,2)", nativeQuery = true)
 	List<POReportDetail> getPOReportDetailList(@Param("poIdList") List<Integer> poIdList);
 
 	@Query(value = "SELECT po_detail.*,i.item_code,i.item_desc as item_name, ih.ind_m_date FROM po_detail ,m_item i ,indtrans id,indent ih WHERE po_detail.po_id=:poId  AND i.item_id=po_detail.item_id AND "
