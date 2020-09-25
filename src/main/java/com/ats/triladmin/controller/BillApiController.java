4694d043b5391712da660eb528505968427ae2ad
maddy <maddy@maddy-Inspiron-3541>
2020-09-25 11:49:22 +0530

    //maddy

--- src/main/java/com/ats/triladmin/controller/BillApiController.java
+++ src/main/java/com/ats/triladmin/controller/BillApiController.java
@@ -17,12 +17,14 @@ import com.ats.triladmin.model.BillBookDetail;
 import com.ats.triladmin.model.BillBookHeader;
 import com.ats.triladmin.model.GetBillHeader;
 import com.ats.triladmin.model.Info;
+import com.ats.triladmin.model.MrnExcelPuch;
 import com.ats.triladmin.model.Vendor;
 import com.ats.triladmin.model.mrn.GetMrnHeaderWithAmt;
 import com.ats.triladmin.model.mrn.MrnDetailForBillBook;
 import com.ats.triladmin.repository.BillBookDetailRepo;
 import com.ats.triladmin.repository.BillBookHeaderRepo;
 import com.ats.triladmin.repository.GetBillHeaderRepo;
+import com.ats.triladmin.repository.MrnExcelPuchRepository;
 import com.ats.triladmin.repository.VendorRepository;
 import com.ats.triladmin.repository.mrn.GetMrnHeaderWithAmtRepository;
 import com.ats.triladmin.repository.mrn.MrnDetailForBillBookRepo;
@@ -46,6 +48,9 @@ public class BillApiController {
 	
 	@Autowired
 	GetBillHeaderRepo getBillHeaderRepo;
+	
+	@Autowired
+	MrnExcelPuchRepository mrnExcelPuchRepository;
 	
 	@RequestMapping(value = { "/getVendorForBillBook" }, method = RequestMethod.POST)
 	public @ResponseBody List<Vendor> getVendorForBillBook() {
@@ -234,5 +239,95 @@ public class BillApiController {
 
 		}
 		
+		@RequestMapping(value = {"/getBillBookExcelReport"}, method = RequestMethod.POST)
+		public @ResponseBody List<MrnExcelPuch> getMrnHsnwiseExcelReport(@RequestParam("mrnIdList") List<String> mrnIdList)
 
+		{
+			List<MrnExcelPuch> report = new ArrayList<MrnExcelPuch>();
+
+			try {
+
+				report = mrnExcelPuchRepository.getBillBookExcelReport(mrnIdList);
+			} catch (Exception e) {
+				System.out.println("Exce In /getBillBookExcelReport " + e.getMessage());
+
+				e.printStackTrace();
+			}
+			return report;
+		}
 }
+
+
+//SELECT
+//t_mrn_detail.mrn_detail_id,
+//t_mrn_detail.mrn_id,
+//m_category.cat_id as mrn_type_id,
+//m_category.cat_id,
+//m_category.cat_desc as mrn_type,
+//t_mrn_header.mrn_no,
+//t_mrn_header.mrn_date,
+//t_mrn_header.bill_no,
+//t_mrn_header.bill_date,
+//po_header.po_no,
+//po_header.po_frt_remark As remark,
+//po_header.po_date,
+//'Purchase' as type,
+//m_vendor.vendor_id,
+//m_vendor.vendor_id as supplier_id,
+//m_vendor.vendor_code as supplier_code,
+//m_vendor.vendor_name as supplier_name,
+//m_vendor.vendor_add4 as supp_erp_code,
+//m_vendor.vendor_gst_no as gst_no,
+//m_vendor.vendor_state as state,
+//m_item.item_id,
+//m_item.item_wt as vendor_erp_code,
+//m_item.item_desc as item_name,
+//m_tax_form.tax_desc as hsn_code,
+//dtl.mrn_qty as qty,
+//dtl.item_uom as uom,
+//dtl.item_rate as rate,
+//(dtl.mrn_qty * dtl.item_rate)AS amount,
+//dtl.sgst as sgst_per,
+//dtl.cgst as cgst_per,
+//dtl.igst as igst_per,
+//0 as cess_per,
+//0 as cess_rs,
+//(((dtl.tax_value/2)/dtl.item_qty)*dtl.mrn_qty) as sgst_rs,
+//(((dtl.tax_value/2)/dtl.item_qty)*dtl.mrn_qty) as cgst_rs ,
+//(((dtl.tax_value)/dtl.item_qty)*dtl.mrn_qty) as igst_rs,
+//dtl.disc_per as item_discount,
+//dtl.disc_value as total_discount,
+//dtl.pack_value,
+//dtl.freight_value,
+//dtl.insu,
+//dtl.other_charges_befor,
+//dtl.other_charges_after  
+//FROM
+//t_mrn_detail,
+//t_mrn_header,
+//po_header,
+//po_detail,
+//m_vendor,
+//m_tax_form,
+//indtrans,
+//indent,
+//m_category,
+//m_item,
+//t_bill_book_header head,
+//t_bill_book_detail dtl
+//WHERE
+// head.bill_id=dtl.bill_id 
+//AND dtl.mrn_detail_id=t_mrn_detail.mrn_detail_id          
+//AND t_mrn_detail.mrn_id=t_mrn_header.mrn_id 
+//AND po_header.po_id=t_mrn_detail.po_id 
+//AND po_detail.po_detail_id=t_mrn_detail.po_detail_id 
+//AND po_header.po_id=po_detail.po_id 
+//AND m_vendor.vendor_id=head.vend_id 
+//AND dtl.item_id=m_item.item_id 
+//AND m_tax_form.tax_id=m_item.item_is_capital         
+//AND indtrans.ind_d_id=po_detail.ind_id 
+//AND indtrans.ind_m_id=indent.ind_m_id 
+//AND indent.cat_id=m_category.cat_id 
+//AND head.bill_id IN(11,12,13,17) 
+//ORDER BY
+//t_mrn_header.mrn_date
--- src/main/java/com/ats/triladmin/repository/MrnExcelPuchRepository.java
+++ src/main/java/com/ats/triladmin/repository/MrnExcelPuchRepository.java
@@ -57,5 +57,84 @@ public interface MrnExcelPuchRepository extends JpaRepository<MrnExcelPuch, Inte
 			"WHERE t_mrn_detail.mrn_id=t_mrn_header.mrn_id AND po_header.po_id=t_mrn_detail.po_id AND po_detail.po_detail_id=t_mrn_detail.po_detail_id AND\n" + 
 			" po_header.po_id=po_detail.po_id AND m_vendor.vendor_id=po_header.vend_id AND t_mrn_detail.item_id=m_item.item_id AND m_tax_form.tax_id=m_item.item_is_capital AND t_mrn_header.mrn_id IN(:mrnIdList) AND indtrans.ind_d_id=po_detail.ind_id AND indtrans.ind_m_id=indent.ind_m_id AND indent.cat_id=m_category.cat_id ORDER BY t_mrn_header.mrn_date",nativeQuery=true)
 	List<MrnExcelPuch> getMrnExcelReport(@Param("mrnIdList")List<String> mrnIdList);
+	
+	/*---------------------------------------------------------------------------------------------------------*/
+	
+	
+	@Query(value="SELECT\n" + 
+			"        t_mrn_detail.mrn_detail_id,\n" + 
+			"        t_mrn_detail.mrn_id,\n" + 
+			"        m_category.cat_id as mrn_type_id,\n" + 
+			"        m_category.cat_id,\n" + 
+			"        m_category.cat_desc as mrn_type,\n" + 
+			"        t_mrn_header.mrn_no,\n" + 
+			"        t_mrn_header.mrn_date,\n" + 
+			"        t_mrn_header.bill_no,\n" + 
+			"        t_mrn_header.bill_date,\n" + 
+			"        po_header.po_no,\n" + 
+			"        po_header.po_frt_remark As remark,\n" + 
+			"        po_header.po_date,\n" + 
+			"        'Purchase' as type,\n" + 
+			"        m_vendor.vendor_id,\n" + 
+			"        m_vendor.vendor_id as supplier_id,\n" + 
+			"        m_vendor.vendor_code as supplier_code,\n" + 
+			"        m_vendor.vendor_name as supplier_name,\n" + 
+			"        m_vendor.vendor_add4 as supp_erp_code,\n" + 
+			"        m_vendor.vendor_gst_no as gst_no,\n" + 
+			"        m_vendor.vendor_state as state,\n" + 
+			"        m_item.item_id,\n" + 
+			"        m_item.item_wt as vendor_erp_code,\n" + 
+			"        m_item.item_desc as item_name,\n" + 
+			"        m_tax_form.tax_desc as hsn_code,\n" + 
+			"        dtl.mrn_qty as qty,\n" + 
+			"        dtl.item_uom as uom,\n" + 
+			"        dtl.item_rate as rate,\n" + 
+			"        (dtl.mrn_qty * dtl.item_rate)AS amount,\n" + 
+			"        dtl.sgst as sgst_per,\n" + 
+			"        dtl.cgst as cgst_per,\n" + 
+			"        dtl.igst as igst_per,\n" + 
+			"        0 as cess_per,\n" + 
+			"        0 as cess_rs,\n" + 
+			"        (((dtl.tax_value/2)/dtl.item_qty)*dtl.mrn_qty) as sgst_rs,\n" + 
+			"        (((dtl.tax_value/2)/dtl.item_qty)*dtl.mrn_qty) as cgst_rs ,\n" + 
+			"        (((dtl.tax_value)/dtl.item_qty)*dtl.mrn_qty) as igst_rs,\n" + 
+			"        dtl.disc_per as item_discount,\n" + 
+			"        dtl.disc_value as total_discount,\n" + 
+			"        dtl.pack_value,\n" + 
+			"        dtl.freight_value,\n" + 
+			"        dtl.insu,\n" + 
+			"        dtl.other_charges_befor,\n" + 
+			"        dtl.other_charges_after  \n" + 
+			"    FROM\n" + 
+			"        t_mrn_detail,\n" + 
+			"        t_mrn_header,\n" + 
+			"        po_header,\n" + 
+			"        po_detail,\n" + 
+			"        m_vendor,\n" + 
+			"        m_tax_form,\n" + 
+			"        indtrans,\n" + 
+			"        indent,\n" + 
+			"        m_category,\n" + 
+			"        m_item,\n" + 
+			"        t_bill_book_header head,\n" + 
+			"        t_bill_book_detail dtl\n" + 
+			"    WHERE\n" + 
+			"    	 head.bill_id=dtl.bill_id \n" + 
+			"        AND dtl.mrn_detail_id=t_mrn_detail.mrn_detail_id          \n" + 
+			"        AND t_mrn_detail.mrn_id=t_mrn_header.mrn_id \n" + 
+			"        AND po_header.po_id=t_mrn_detail.po_id \n" + 
+			"        AND po_detail.po_detail_id=t_mrn_detail.po_detail_id \n" + 
+			"        AND po_header.po_id=po_detail.po_id \n" + 
+			"        AND m_vendor.vendor_id=head.vend_id \n" + 
+			"        AND dtl.item_id=m_item.item_id \n" + 
+			"        AND m_tax_form.tax_id=m_item.item_is_capital         \n" + 
+			"        AND indtrans.ind_d_id=po_detail.ind_id \n" + 
+			"        AND indtrans.ind_m_id=indent.ind_m_id \n" + 
+			"        AND indent.cat_id=m_category.cat_id \n" + 
+			"        AND t_mrn_header.mrn_id IN (:mrnIdList) \n" + 
+			"    ORDER BY\n" + 
+			"        t_mrn_header.mrn_date",nativeQuery=true)	
+	List<MrnExcelPuch> getBillBookExcelReport(@Param("mrnIdList")List<String> mrnIdList);
+	
 
 }
